226e6de3caa4bbc5690b6f4207f3a426559602a2
nikhilmrp <enterprises.mrp25@gmail.com>
2026-06-24 21:27:51 +0530

    vendor profile step fixed

--- src/services/vendorProfileService.ts
+++ src/services/vendorProfileService.ts
@@ -1,5 +1,14 @@
 import sequelize from "@config/database";
-import { CreatePricingDetailsDto, CreateServiceDetailsDto, CreateVendorProfileDto, PricingDetailsResponseDto, ServiceDetailsResponseDto, UploadWorkGalleryDto, UploadWorkGalleryResponseDto, VendorProfileResponseDto } from "@dto/profile.dto";
+import {
+    CreatePricingDetailsDto,
+    CreateServiceDetailsDto,
+    CreateVendorProfileDto,
+    PricingDetailsResponseDto,
+    ServiceDetailsResponseDto,
+    UploadWorkGalleryDto,
+    UploadWorkGalleryResponseDto,
+    VendorProfileResponseDto,
+} from "@dto/profile.dto";
 import locationRepository from "@repositories/config/location.repository";
 import vendorcategoryRepository from "@repositories/config/vendorcategory.repository";
 import vendortypeRepository from "@repositories/config/vendortype.repository";
@@ -22,7 +31,10 @@ class VendorProfileService {
             throw ApiError.notFound("Some of the service locations are not found");
         }
         return sequelize.transaction(async (tx) => {
-            const vendorProfile = await vendorProfileRepository.create({ user_id, business_name, description, address, phone_number, email }, tx);
+            const vendorProfile = await vendorProfileRepository.create(
+                { user_id, business_name, description, address, phone_number, email },
+                tx,
+            );
             const vendorProfileLocations = await VendorProfileLocationRepository.bulkCreateForVendorProfile(
                 vendorProfile.id,
                 service_locations,
@@ -37,7 +49,7 @@ class VendorProfileService {
                 description: vendorProfile.description,
             };
         });
-    }
+    };
 
     createServiceDetails = async (data: CreateServiceDetailsDto): Promise<Omit<ServiceDetailsResponseDto, "id">> => {
         const { user_id, vendor_type_id, vendor_categoryids } = data;
@@ -53,27 +65,34 @@ class VendorProfileService {
         }
 
         const vendorCategories = await vendorcategoryRepository.findByIds(vendor_categoryids);
-        
-        const vendorCategoriesBelongsToVendorType = vendorCategories.every((category) => category.vendor_type_id === vendor_type_id);
 
-        if(!vendorCategoriesBelongsToVendorType) {
+        const vendorCategoriesBelongsToVendorType = vendorCategories.every(
+            (category) => category.vendor_type_id === vendor_type_id,
+        );
+
+        if (!vendorCategoriesBelongsToVendorType) {
             throw ApiError.badRequest("Some of the categories are not belongs to the vendor type");
         }
 
-        if(vendorProfile.profile_step !== 2) {
+        if (vendorProfile.profile_step !== 1) {
             throw ApiError.badRequest("Complete business details first");
         }
 
         const existingServiceDetails = await vendorProfileCategoryRepository.findByVendorProfileId(vendorProfile.id);
         const existingCategoryIdsForUser = existingServiceDetails.map((s) => s.vendor_category_id);
-        const alreadyAddedCategoryIds = vendor_categoryids.filter((categoryId) => existingCategoryIdsForUser.includes(categoryId));
-        if(alreadyAddedCategoryIds.length > 0) {
+        const alreadyAddedCategoryIds = vendor_categoryids.filter((categoryId) =>
+            existingCategoryIdsForUser.includes(categoryId),
+        );
+        if (alreadyAddedCategoryIds.length > 0) {
             throw ApiError.conflict("Some of the categories are already added");
         }
 
         return sequelize.transaction(async (tx) => {
-            await vendorProfileRepository.update({ vendor_type_id, id: vendorProfile.id },tx);
-            const serviceDetails = await vendorProfileCategoryRepository.create({ vendor_profile_id: vendorProfile.id, vendor_categoryids }, tx);
+            await vendorProfileRepository.update({ vendor_type_id, id: vendorProfile.id }, tx);
+            const serviceDetails = await vendorProfileCategoryRepository.create(
+                { vendor_profile_id: vendorProfile.id, vendor_categoryids },
+                tx,
+            );
             await vendorProfileRepository.updateProfileStep(user_id, 2, false, tx);
             return {
                 vendor_profile_id: vendorProfile.id,
@@ -81,19 +100,22 @@ class VendorProfileService {
                 vendor_categoryids: serviceDetails.map((s) => s.vendor_category_id),
             };
         });
-    }
+    };
 
     createPricingDetails = async (data: CreatePricingDetailsDto): Promise<PricingDetailsResponseDto> => {
         const { user_id, pricing_details, amount } = data;
-       const vendorProfile = await vendorProfileRepository.findByUserId(user_id);
+        const vendorProfile = await vendorProfileRepository.findByUserId(user_id);
         if (!vendorProfile) {
             throw ApiError.notFound("Vendor profile not found");
         }
-        if(vendorProfile.profile_step !== 3) {
+        if (vendorProfile.profile_step !== 2) {
             throw ApiError.badRequest("Complete service details first");
         }
         return sequelize.transaction(async (tx) => {
-            const pricingDetails = await vendorPricingRepository.create({ vendor_profile_id: vendorProfile.id, pricing_details, amount }, tx);
+            const pricingDetails = await vendorPricingRepository.create(
+                { vendor_profile_id: vendorProfile.id, pricing_details, amount },
+                tx,
+            );
             await vendorProfileRepository.updateProfileStep(user_id, 3, false, tx);
             return {
                 vendor_profile_id: vendorProfile.id,
@@ -104,7 +126,7 @@ class VendorProfileService {
                 })),
             };
         });
-    }
+    };
 
     uploadWorkGalleryImages = async (data: UploadWorkGalleryDto): Promise<UploadWorkGalleryResponseDto> => {
         const { user_id, image_urls } = data;
@@ -113,14 +135,23 @@ class VendorProfileService {
             throw ApiError.notFound("Vendor profile not found");
         }
         return sequelize.transaction(async (tx) => {
-            const workGalleryImages = await VendorGalleryRespository.bulkCreate(image_urls.map((image_url) => ({ vendor_profile_id: vendorProfile.id, image_url })), tx);
+            const workGalleryImages = await VendorGalleryRespository.bulkCreate(
+                image_urls.map((image_url) => ({
+                    vendor_profile_id: vendorProfile.id,
+                    image_url,
+                })),
+                tx,
+            );
             await vendorProfileRepository.updateProfileStep(user_id, 4, false, tx);
             return {
                 vendor_profile_id: vendorProfile.id,
-                image_urls: workGalleryImages.map((w) => ({ id: w.id, image_url: w.image_url })),
+                image_urls: workGalleryImages.map((w) => ({
+                    id: w.id,
+                    image_url: w.image_url,
+                })),
             };
         });
-    }
+    };
 }
 
 export default new VendorProfileService();
