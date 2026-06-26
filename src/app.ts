fd2d54dab8a185e72e5801841e966ca9e0335d8c
nikhilmrp <enterprises.mrp25@gmail.com>
2026-06-26 14:35:24 +0530

    authLimitter changed for dev mode

--- src/app.ts
+++ src/app.ts
@@ -63,7 +63,7 @@ const apiLimiter = rateLimit({
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: process.env.NODE_ENV === "development" ? 100 : 5,
   message: "Too many login attempts, please try again later.",
   skipSuccessfulRequests: true,
 });
